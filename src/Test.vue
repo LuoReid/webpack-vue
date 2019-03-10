2bd44e28f5779b4f70943f11b067da176b3e2384
Luo Reid <luo.reid@gmail.com>
2019-03-10 18:09:28 +0800

    css init

--- src/Test.vue
+++ src/Test.vue
@@ -8,15 +8,28 @@ export default {
   },
   render(h) {
     const itemClass = "movie";
-    return <ul class="moves">
-      { this.movies.map(move => 
-        <li class={itemClass}>{move.title}</li>
-      )}
-    </ul>
+    return (
+      <ul class="movies">
+        {this.movies.map(move => (
+          <li class={itemClass}>{move.title}</li>
+        ))}
+      </ul>
+    );
   }
 };
 </script>
 <style scoped>
+.movies {
+  list-style: none;
+  padding: 12px;
+  background: rgba(0, 0, 0, 0.1);
+  border-radius: 3px;
+}
+.movie:not(:last-child) {
+  padding-bottom: 6px;
+  margin-bottom: 6px;
+  border-bottom: solid 1px rgba(0, 0, 0, 0.1);
+}
 p {
   color: grey;
 }
