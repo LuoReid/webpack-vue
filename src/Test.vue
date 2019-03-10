ed79782316d37ca7274918566917a4a5392f733a
Luo Reid <luo.reid@gmail.com>
2019-03-10 18:26:37 +0800

    stylus loader

--- src/Test.vue
+++ src/Test.vue
@@ -23,22 +23,22 @@ export default {
   }
 };
 </script>
-<style lang="less" scoped>
-.movies {
-  list-style: none;
-  padding: 12px;
-  background: rgba(0, 0, 0, 0.1);
-  border-radius: 3px;
-  .movie:not(:last-child) {
-    padding-bottom: 6px;
-    margin-bottom: 6px;
-    border-bottom: solid 1px rgba(0, 0, 0, 0.1);
-  }
-}
+<style lang="stylus" scoped>
+// .movies {
+//   list-style: none;
+//   padding: 12px;
+//   background: rgba(0, 0, 0, 0.1);
+//   border-radius: 3px;
+//   .movie:not(:last-child) {
+//     padding-bottom: 6px;
+//     margin-bottom: 6px;
+//     border-bottom: solid 1px rgba(0, 0, 0, 0.1);
+//   }
+// }
 
-.article {
-  .title {
-    border-bottom: solid 3px fade(red, 20%);
-  }
-}
+.article 
+  .title 
+    border-bottom solid 3px rgba(red, .3)
+  
+
 </style>
