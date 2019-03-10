8cd544381ee580616c270db80d3abd821ee586b1
Luo Reid <luo.reid@gmail.com>
2019-03-10 18:19:08 +0800

    sass loader

--- src/Test.vue
+++ src/Test.vue
@@ -9,28 +9,33 @@ export default {
   render(h) {
     const itemClass = "movie";
     return (
-      <ul class="movies">
-        {this.movies.map(move => (
-          <li class={itemClass}>{move.title}</li>
-        ))}
-      </ul>
+      <div>
+        <ul class="movies">
+          {this.movies.map(move => (
+            <li class={itemClass}>{move.title}</li>
+          ))}
+        </ul>
+        <article class="article">
+          <h3 class="title">Title</h3>
+        </article>
+      </div>
     );
   }
 };
 </script>
-<style scoped>
-.movies {
+<style lang="sass" scoped>
+.movies 
   list-style: none;
   padding: 12px;
   background: rgba(0, 0, 0, 0.1);
-  border-radius: 3px;
-}
-.movie:not(:last-child) {
-  padding-bottom: 6px;
-  margin-bottom: 6px;
-  border-bottom: solid 1px rgba(0, 0, 0, 0.1);
-}
-p {
-  color: grey;
-}
+  border-radius: 3px;  
+  .movie:not(:last-child) 
+    padding-bottom: 6px;
+    margin-bottom: 6px;
+    border-bottom: solid 1px rgba(0, 0, 0, 0.1);
+
+
+.article
+  .title
+    border-bottom: solid 3px rgba(red,.2)    
 </style>
